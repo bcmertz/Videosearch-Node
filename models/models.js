76a818d7139e398b53bdd399e07d25fd6aa905f6
thistle <bcmertz@email.wm.edu>
2018-09-13 13:08:33 -0400

    s

--- models/models.js
+++ models/models.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 var connect = process.env.MONGODB_URI
-mongoose.connect(connect);
+mongoose.connect(connect, {useMongoClient: true});
 
 var Frame = new mongoose.Schema({
   predictions: Array,
