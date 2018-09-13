6fa7b774410458283102602fb21f035c76396bc7
thistle <bcmertz@email.wm.edu>
2018-09-13 14:52:33 -0400

    s

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,9 @@ app.use(express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-app.use((req,res) => {
+app.use((req,res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    next()
 });
 app.use('/', routes);
 
